16591d74f7f301b9a34f6bf6aff2e58c8174211f
nayeonkiim <61819669+nayeonkiim@users.noreply.github.com>
2021-09-16 11:49:12 +0900

    Revert "<#9> Feat: notice,qna,faq,company 엔티티 추가"

--- src/main/java/com/linkerbell/portradebackend/domain/admin/domain/Notice.java
+++ src/main/java/com/linkerbell/portradebackend/domain/admin/domain/Notice.java
@@ -1,43 +1,4 @@
 package com.linkerbell.portradebackend.domain.admin.domain;
 
-import com.linkerbell.portradebackend.domain.user.domain.User;
-import com.linkerbell.portradebackend.global.common.BaseTimeEntity;
-import lombok.*;
-import org.hibernate.annotations.ColumnDefault;
-
-import javax.persistence.*;
-import java.time.LocalDateTime;
-
-@Entity
-@Getter
-@ToString(of = {"id","title","viewCount"})
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
-@Table(name = "notice")
-public class Notice extends BaseTimeEntity {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "notice_id")
-    private Long id;
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id")
-    private User user;
-
-    @Column(nullable = false)
-    private String title;
-
-    @Column(name = "view_count")
-    private int viewCount = 0;
-
-    @Column(name = "last_modified_date")
-    private LocalDateTime lastModifiedDate = LocalDateTime.now();
-
-    @Builder
-    public Notice(Long id, User user, String title, int viewCount) {
-        this.id = id;
-        this.user = user;
-        this.title = title;
-        this.viewCount = viewCount;
-    }
+public class Notice {
 }
