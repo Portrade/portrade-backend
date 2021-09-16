0686bc4bd8af10e964c4f0e0d307190a608f83d3
nayeonkiim <wkdrnsldk98@naver.com>
2021-09-16 11:25:30 +0900

    <#9> Refactor: 디렉토리 위치 변경

--- src/main/java/com/linkerbell/portradebackend/domain/admin/domain/Faq.java
+++ src/main/java/com/linkerbell/portradebackend/domain/admin/domain/Faq.java
@@ -1,4 +1,38 @@
 package com.linkerbell.portradebackend.domain.admin.domain;
 
-public class Faq {
+import com.linkerbell.portradebackend.domain.user.domain.User;
+import com.linkerbell.portradebackend.global.common.BaseTimeEntity;
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@ToString(of = {"id", "title"})
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "faq")
+public class Faq extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "faq_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(name = "last_modified_date")
+    private LocalDateTime lastModifiedDate = LocalDateTime.now();
+
+    @Builder
+    public Faq(Long id, User user, String title) {
+        this.id = id;
+        this.user = user;
+        this.title = title;
+    }
 }
