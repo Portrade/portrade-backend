104756e65e05d1f9d09f422415690aa50f6e734c
nayeonkiim <wkdrnsldk98@naver.com>
2021-10-01 11:33:50 +0900

    <#48> Refactor: 인가,인증 아직 안됨

--- src/test/java/com/linkerbell/portradebackend/domain/qna/controller/QnaControllerTest.java
+++ src/test/java/com/linkerbell/portradebackend/domain/qna/controller/QnaControllerTest.java
@@ -11,12 +11,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithAnonymousUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -74,12 +76,13 @@ class QnaControllerTest {
     }
 
     @Test
+    @WithAnonymousUser
     @DisplayName("1:1 문의 등록 API 실패")
     public void test_anonymous() throws Exception {
 
         mvc= MockMvcBuilders
-                .standaloneSetup(qnaController)
-                .setCustomArgumentResolvers(new PrincipalDetailsArgumentResolver(null))
+                .webAppContextSetup(context)
+                .apply(springSecurity())
                 .build();
 
         //given
