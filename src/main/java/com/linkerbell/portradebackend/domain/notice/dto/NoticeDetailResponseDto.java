4c49adcc91ff549b3b8972721d87601fd999870c
Jaster25 <js_328@naver.com>
2021-10-05 20:36:03 +0900

    <#44> Feat: API 반환 값 불변성 속성 추가

--- src/main/java/com/linkerbell/portradebackend/domain/notice/dto/NoticeDetailResponseDto.java
+++ src/main/java/com/linkerbell/portradebackend/domain/notice/dto/NoticeDetailResponseDto.java
@@ -1,26 +1,23 @@
 package com.linkerbell.portradebackend.domain.notice.dto;
 
-import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Getter
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class NoticeDetailResponseDto {
 
-    private Long id;
-    private String creator;
-    private String title;
-    private String content;
-    private int viewCount;
-    private LocalDateTime createdDate;
-    private LocalDateTime lastModifiedDate;
+    private final Long id;
+    private final String creator;
+    private final String title;
+    private final String content;
+    private final int viewCount;
+    private final LocalDateTime createdDate;
+    private final LocalDateTime lastModifiedDate;
 
-    private NoticeResponseDto next;
-    private NoticeResponseDto prev;
+    private final NoticeResponseDto next;
+    private final NoticeResponseDto prev;
 
     @Builder
     public NoticeDetailResponseDto(Long id, String creator, String title, String content, int viewCount, LocalDateTime createdDate, LocalDateTime lastModifiedDate, NoticeResponseDto next, NoticeResponseDto prev) {
--- src/main/java/com/linkerbell/portradebackend/domain/notice/dto/NoticeResponseDto.java
+++ src/main/java/com/linkerbell/portradebackend/domain/notice/dto/NoticeResponseDto.java
@@ -1,22 +1,19 @@
 package com.linkerbell.portradebackend.domain.notice.dto;
 
 import com.linkerbell.portradebackend.domain.notice.domain.Notice;
-import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Getter
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class NoticeResponseDto {
 
-    private Long id;
-    private String creator;
-    private String title;
-    private int viewCount;
-    private LocalDateTime createdDate;
+    private final Long id;
+    private final String creator;
+    private final String title;
+    private final int viewCount;
+    private final LocalDateTime createdDate;
 
     @Builder
     public NoticeResponseDto(Long id, String creator, String title, int viewCount, LocalDateTime createdDate) {
