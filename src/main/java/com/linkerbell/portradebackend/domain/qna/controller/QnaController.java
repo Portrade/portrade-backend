20652b4904d98bfa4e6dca7b390f17d87866e809
Jaster25 <js_328@naver.com>
2021-10-05 18:17:14 +0900

    <#44> Feat: Swagger 공지사항 API 추가

--- src/main/java/com/linkerbell/portradebackend/domain/qna/controller/QnaController.java
+++ src/main/java/com/linkerbell/portradebackend/domain/qna/controller/QnaController.java
@@ -1,6 +1,9 @@
 package com.linkerbell.portradebackend.domain.qna.controller;
 
-import com.linkerbell.portradebackend.domain.qna.dto.*;
+import com.linkerbell.portradebackend.domain.qna.dto.CreateQnaRequestDto;
+import com.linkerbell.portradebackend.domain.qna.dto.QnaDetailResponseDto;
+import com.linkerbell.portradebackend.domain.qna.dto.QnasResponseDto;
+import com.linkerbell.portradebackend.domain.qna.dto.ReplyQnaRequestDto;
 import com.linkerbell.portradebackend.domain.qna.service.QnaService;
 import com.linkerbell.portradebackend.domain.user.domain.User;
 import com.linkerbell.portradebackend.global.common.annotation.CurrentUser;
@@ -37,7 +40,7 @@ public class QnaController {
     @Operation(summary = "1:1 문의 답변 등록", description = "1:1 문의 게시글의 답변을 등록한다.")
     @PostMapping("/{qnaId}/answer")
     public ResponseEntity<CreateResponseDto> createAnswerApi(
-            @Parameter(description="답변 할 게시글 id") @PathVariable("qnaId") Long qnaId,
+            @Parameter(description = "답변 할 게시글 id") @PathVariable("qnaId") Long qnaId,
             @RequestBody @Valid ReplyQnaRequestDto requestDto,
             @Parameter(hidden = true) @CurrentUser User user) {
         CreateResponseDto createQnaResponseDto = qnaService.createAnswer(qnaId, requestDto, user);
@@ -47,8 +50,8 @@ public class QnaController {
     @Operation(summary = "1:1 문의 게시글 목록 조회", description = "1:1 문의 게시글 목록을 조회한다.")
     @GetMapping
     public ResponseEntity<QnasResponseDto> getQnasApi(
-            @Parameter(description="페이지 번호") @RequestParam(value = "page", defaultValue = "1") int page,
-            @Parameter(description="반환할 데이터 수") @RequestParam(value = "size", defaultValue = "10") int size) {
+            @Parameter(description = "페이지 번호") @RequestParam(value = "page", defaultValue = "1") int page,
+            @Parameter(description = "반환할 데이터 수") @RequestParam(value = "size", defaultValue = "10") int size) {
         QnasResponseDto qnasResponseDto = qnaService.getQnas(page, size);
         return ResponseEntity.status(HttpStatus.OK).body(qnasResponseDto);
     }
@@ -56,8 +59,8 @@ public class QnaController {
     @Operation(summary = "1:1 문의 게시글 상세 조회", description = "1:1 문의 게시글을 상세 조회한다.")
     @GetMapping("/{qnaId}")
     public ResponseEntity<QnaDetailResponseDto> getQnaDetailApi(
-            @Parameter(description="조회 할 게시글 id") @PathVariable("qnaId") Long qnaId,
-            @Parameter(hidden = true) @CurrentUser User user){
+            @Parameter(description = "조회 할 게시글 id") @PathVariable("qnaId") Long qnaId,
+            @Parameter(hidden = true) @CurrentUser User user) {
         QnaDetailResponseDto qnaDetailResponseDto = qnaService.getQna(qnaId, user);
         return ResponseEntity.status(HttpStatus.OK).body(qnaDetailResponseDto);
     }
